c68bea0cae6ec5c011a14fbf6a96539914bb564f
root <root@ip-172-31-61-200.ec2.internal>
2020-08-22 19:51:47 +0000

    Updating sync script with different file directories

--- sync_website_with_github.sh
+++ sync_website_with_github.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# 2020-03-18
+# 2020-08-22
 # Use this script to sync the website location with changes made in the GitHub directory
 # and reload nginx.
 
-# Custom website directory. Change at /etc/nginx/sites-enabled/default on Line 41.
-website_dir=/srv/www/zeleena/
-github_dir=/home/zeleena/zeleena.github.io/
+# Custom website directory. Change at /etc/nginx/nginx.conf.
+website_dir=/var/www/zeleena.com/public_html
+github_dir=/var/www/zeleena.com/zeleena.github.io/
 
 sudo rsync -av --exclude='*.sh' --exclude=".*" --del $github_dir $website_dir
 
