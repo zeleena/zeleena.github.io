5ad639bea3b88e50ef8be707ab0f082d88b42c49
Zeleena Kearney <12972510+zeleena@users.noreply.github.com>
2023-01-04 22:15:46 -0800

    Update home path and link helpful article

--- sync_website_with_github.sh
+++ sync_website_with_github.sh
@@ -2,10 +2,13 @@
 # 2020-08-22
 # Use this script to sync the website location with changes made in the GitHub directory
 # and reload nginx.
+#
+# This guide was helpful when setting up / migrating:
+# https://jgefroh.medium.com/a-guide-to-using-nginx-for-static-websites-d96a9d034940
 
 # Custom website directory. Change at /etc/nginx/nginx.conf.
 website_dir=/var/www/zeleena.com/public_html
-github_dir=/home/ec2-user/zeleena.github.io/
+github_dir=/home/zeleenak/zeleena.github.io
 
 sudo rsync -av --exclude='*.sh' --exclude=".*" --del $github_dir $website_dir
 
